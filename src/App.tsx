538e2fa319cff44d454d72f9435ba33af92ef82e
Matthew Lee <matthewlee0725@gmail.com>
2023-08-05 15:02:23 +0800

    feat: show google oauth button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
+
+import { GoogleLogin } from '@react-oauth/google'
 
 function App() {
   return (
-    <Container maxWidth='xs'>
-      <Box>
-        {/* TODO */}
-      </Box>
+    <Container
+      maxWidth='xs'
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        pt: 8,
+      }}
+    >
+      <GoogleLogin
+        onSuccess={response => {
+          // TODO
+        }}
+        onError={() => {
+          // TODO
+        }}
+        width={336}
+      />
     </Container>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
 import App from './App'
 import './index.css'
 
@@ -10,6 +12,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId='304531247476-58f940f3b0dgrupg95cdo8b51fspupdv.apps.googleusercontent.com'>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 )
