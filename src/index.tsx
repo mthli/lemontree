a57ac1786ecc0f67d586b573486a82f1dbc1294b
Matthew Lee <matthewlee0725@gmail.com>
2023-08-14 21:32:12 +0800

    feat: create router /

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { GOOGLE_OAUTH_CLIENT_ID } from './constants'
@@ -7,14 +8,21 @@ import { GOOGLE_OAUTH_CLIENT_ID } from './constants'
 import App from './App'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
 
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={GOOGLE_OAUTH_CLIENT_ID}>
-      <App />
+      <RouterProvider router={router} />
     </GoogleOAuthProvider>
   </React.StrictMode>
 )
